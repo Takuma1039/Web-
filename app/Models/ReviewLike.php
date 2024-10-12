a9429c8f04ad2882eef3f2e2c195461616afd18d
Takuma1039 <j241a031@gunma-u.ac.jp>
2024-10-12 14:19:36 +0900

    コードの修正

--- app/Models/ReviewLike.php
+++ app/Models/ReviewLike.php
@@ -9,6 +9,8 @@ class ReviewLike extends Model
 {
     use HasFactory;
     
+    public $incrementing = false;
+    
     protected $fillable = ['user_id', 'review_id'];
 
     public function user()
