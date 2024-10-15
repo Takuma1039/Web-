a99b481ffb27191fce7a5933d4402e4fcb213f4a
Takuma1039 <j241a031@gunma-u.ac.jp>
2024-10-15 23:50:31 +0900

    コードの修正

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -35,7 +35,7 @@
 
 
                 <!-- Windowbar ボタン -->
-                <button class="text-gray-500 hover:text-gray-600" id="open-sidebar" aria-expanded="false" aria-controls="sidebar" @click="open = !open">
+                <button class="text-gray-500 hover:text-gray-600 touch-manipulation" id="open-sidebar" aria-expanded="false" aria-controls="sidebar" @click="open = !open">
                     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
                     </svg>
@@ -131,13 +131,6 @@
             sidebar.classList.toggle('-translate-x-full', isOpen);
         });
 
-        // キーボード操作の追加
-        sidebarToggle.addEventListener('keydown', (event) => {
-            if (event.key === 'Enter' || event.key === ' ') {
-                sidebarToggle.click();
-                event.preventDefault();
-            }
-        });
 
         // サイドバー外をクリックした場合、サイドバーを閉じる
         document.addEventListener('click', (event) => {
